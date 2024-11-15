2a08a5899b8c91a99c1dbde3822f357807501129
Kristina Tereshchenko <kristovinia@gmail.com>
2024-11-15 13:00:03 +0100

    Currency + Account

--- src/model/Account.java
+++ src/model/Account.java
@@ -1,11 +1,71 @@
 package model;
 
-import java.util.Map;
+//repository - map<Integer, List<Account>>, Integer - user id
+//repository - map <Integer, List<Transaction>>, id - account id
+
+
+import java.util.Objects;
 
 public class Account {
-    //private Map<Currency, Transaction> account;
-    // balance, currency, user id, account id
 
-    //repository - map<Integer, List<Account>>, Integer - user id
-    //repository - map <Integer, List<Transaction>>, id - account id
+    private final int userId;
+    private final int accountId;
+    private double balance;
+    private Currency currency;
+
+    public Account(int accountId, double balance, Currency currency, int userId) {
+        this.accountId = accountId;
+        this.balance = balance;
+        this.currency = currency;
+        this.userId = userId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Account account = (Account) o;
+        return userId == account.userId && accountId == account.accountId && Double.compare(balance, account.balance) == 0 && Objects.equals(currency, account.currency);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, accountId, balance, currency);
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void setCurrency(Currency currency) {
+        this.currency = currency;
+    }
+
+
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "userId=" + userId +
+                ", accountId=" + accountId +
+                ", balance=" + balance +
+                ", currency='" + currency + '\'' +
+                '}';
+    }
 }
--- src/model/Currency.java
+++ src/model/Currency.java
@@ -1,7 +1,50 @@
 package model;
 
+import java.util.Objects;
+
 public class Currency {
     String currencyCode;
     String title;
-    //
+
+    public Currency(String currencyCode, String title) {
+        this.currencyCode = currencyCode;
+        this.title = title;
+    }
+
+    public String getCurrencyCode() {
+        return currencyCode;
+    }
+
+    public void setCurrencyCode(String currencyCode) {
+        this.currencyCode = currencyCode;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Currency currency = (Currency) o;
+        return Objects.equals(currencyCode, currency.currencyCode) && Objects.equals(title, currency.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(currencyCode, title);
+    }
+
+    @Override
+    public String toString() {
+        return "Currency{" +
+                "currencyCode='" + currencyCode + '\'' +
+                ", title='" + title + '\'' +
+                '}';
+    }
 }
