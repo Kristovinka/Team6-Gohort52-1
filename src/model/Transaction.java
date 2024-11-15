5a81f14634932782c7d2a23059ff039737081860
Kristina Tereshchenko <kristovinia@gmail.com>
2024-11-15 11:31:15 +0100

    model

---
+++ src/model/Transaction.java
@@ -0,0 +1,10 @@
+package model;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Transaction {
+
+    private double exchange_Rate;
+    private LocalDateTime time;
+}
