86845e5fc19b84d51e9437696a6e6c9ad6242ece
KKibirieva <kkibirieva@gmail.com>
2024-11-18 12:03:01 +0200

    user repo done

--- src/repository/UserRepository.java
+++ src/repository/UserRepository.java
@@ -1,17 +1,19 @@
 package repository;
 
+import model.Role;
 import model.Transaction;
 import model.User;
 
 import java.util.List;
+import java.util.Map;
 
 public interface UserRepository {
 
-    User addUser(String email, String password);
+    User addUser(String email, String password, Role role);
 
     boolean doesEmailExist(String email);
 
-    List<User> getAllUsers(); // Метод для получения всех пользователей
+    Map<Integer,User> getAllUsers(); // Метод для получения всех пользователей
 
     List<Transaction> getTransactionsByUser(User user); // Метод позволят получать операции пользователя
 
--- src/repository/UserRepositoryImpl.java
+++ src/repository/UserRepositoryImpl.java
@@ -1,33 +1,62 @@
 package repository;
 
+import model.Role;
 import model.Transaction;
 import model.User;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class UserRepositoryImpl implements UserRepository{
+
+    private final Map<Integer, User> users;
+    private AtomicInteger usersCount = new AtomicInteger(1);
+
+    public UserRepositoryImpl() {
+        users = new HashMap<Integer, User>();
+        addUsers();
+    }
+
+    private void addUsers() {
+        User admin = new User("1", "1", Role.ADMIN);
+        admin.setRole(Role.ADMIN);
+
+        User blocked = new User("2", "2", Role.ADMIN);
+        blocked.setRole(Role.BLOCKED);
+        users.put(usersCount.getAndIncrement(), admin);
+        users.put(usersCount.getAndIncrement(),blocked);
+        users.put(usersCount.getAndIncrement(),new User("user@email.com", "qwerty1Q!", Role.ADMIN));
+    }
+
     @Override
-    public User addUser(String email, String password) {
-        return null;
+    public User addUser(String email, String password, Role role) {
+        User user = new User(email, password, role);
+        users.put(usersCount.getAndIncrement(),user);
+        return user;
     }
 
     @Override
     public boolean doesEmailExist(String email) {
+        for (Map.Entry<Integer, User> entry : users.entrySet()) {
+            if (entry.getValue().getEmail().equals(email)) return true;
+        }
         return false;
     }
 
     @Override
-    public List<User> getAllUsers() {
-        return List.of();
+    public Map<Integer,User> getAllUsers() {
+        return users;
     }
 
     @Override
     public List<Transaction> getTransactionsByUser(User user) {
-        return List.of();
+        return user.getUserTransactions();
     }
 
     @Override
     public void clearAllUsers() {
-
+        users.clear();
     }
 }
